c87ef1f236910ea82bf4110a45923da887e51567
BPOTW <133132057+BPOTW@users.noreply.github.com>
2025-09-25 11:13:55 +0500

    commit

---
+++ script.js
@@ -0,0 +1,36 @@
+async function getQuery() {
+  try {
+    const input = document.getElementById("inputText").value;
+    const resultDiv = document.querySelector("#geminiAns");
+    const result = await axios.post(
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",{
+    "contents": [
+      {
+        "parts": [
+            {
+                "text":"You are an expert python programmer who only knows about python. You only answers about python code and if user asks other queries just say I don't know."
+            },
+          {
+            "text": input
+          }
+        ]
+      }
+    ]
+  },{
+    headers:{
+    "X-goog-api-key":"AIzaSyDSrXvCZxdja7LOrweXYXoJXBRZbCzzrh8",
+    "Content-Type":"application/json"
+  }
+  }
+    );
+    resultDiv.innerHTML = `Gemini ans is : ${result.data.candidates[0].content.parts[0].text}`;
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong. Please try again")
+  }
+}
+
+// checkWether()
+// setInterval(() => {
+//     checkWether();
+// }, 5000);
